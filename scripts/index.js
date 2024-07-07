3e0e501aa160a0daa99e043751a51ec5b8839271
Sofiia Gudenko <grandpoopris@yandex.ru>
2024-07-07 22:27:13 +0700

    Добавлен JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.cloneNode(true).querySelector(".card");
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, deleteCard);
+  placesList.appendChild(cardElement);
+});
